af372c4fb33c9ab483150214883d95ffb6ec7b94
kamakshis <kamakshis230@gmail.com>
2023-09-24 11:21:18 -0400

    Camp-us second commit

--- app/src/main/java/com/firstapp/campusbuddyv3/RouteActivity.java
+++ app/src/main/java/com/firstapp/campusbuddyv3/RouteActivity.java
@@ -2,9 +2,13 @@ package com.firstapp.campusbuddyv3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class RouteActivity extends AppCompatActivity {
 
@@ -13,7 +17,24 @@ public class RouteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route);
 
+
+
+
+        Button submitbtn = (Button) findViewById(R.id.submit_button);
+
+        submitbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent intent = new Intent(RouteActivity.this, ResultsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
         Spinner spinner = (Spinner) findViewById(R.id.schools_spinner);
+
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.schools_array, android.R.layout.simple_spinner_item);
@@ -21,5 +42,38 @@ public class RouteActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+
+
+
+        Spinner spinner_loc = (Spinner) findViewById(R.id.location_spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter_loc = ArrayAdapter.createFromResource(this,
+                R.array.location_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter_loc.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner_loc.setAdapter(adapter_loc);
+
+
+        Spinner spinner_dest = (Spinner) findViewById(R.id.destination_spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter_dest = ArrayAdapter.createFromResource(this,
+                R.array.destination_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter_dest.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner_dest.setAdapter(adapter_dest);
+
+
+
+        Spinner spinner_notif = (Spinner) findViewById(R.id.notify_spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter_notif = ArrayAdapter.createFromResource(this,
+                R.array.notify_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter_notif.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner_notif.setAdapter(adapter_notif);
     }
 }
